84c16387218ca16e91946af8b8df49dd08836bcc
ritika <ritika@mozzarella.ict.kth.se>
2023-03-04 10:46:13 +0100

    more changes

--- code/model.py
+++ code/model.py
@@ -1,29 +1,12 @@
+#This class interprets the instruction read by assembly
+from ISA import ISA
+
 class Model():
+
     def __init__(self):
-        self._components = {comp1, comp2, ..}
-        self._instructions = {instr1, instr2, ..}
-        self._active_components = {{instr1, {comp1, comp2}}, {instr1, {comp1, comp2}}}
-        self._active_component_cycles = {{instr, {comp1, cycle}, {comp2, cycle}}, {instr, {comp1, cycle}, {comp2, cycle}}}
-        self._instr_energy_equations = {}
-        self._model_file = {}
+        self.ISA = ISA()
 
-    def set_components():
-        self._components +=
-
-    def set_instructions():
-        self._instructions +=
-
-    def set_component_active_cycles_instr():
-        for instr in self._instructions:
-            self._component_active_cycles_instr += {instr, {component, cycles}}
-
-    def set_component_active_cycles():
-        for instr in self._instructions:
-            self._active_component_cycles_instr += {instr, {component, cycles}}
-
-    def get_active_component(instr):
-
-    def get_active_component_cycles(instr):
-
-        
-
+    def set_model(self,name, attribute_values):
+        self.ISA.attributes[name] = attribute_values
+        self.ISA.set_active_cycles()
+Model()
