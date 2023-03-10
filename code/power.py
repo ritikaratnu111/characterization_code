d4e6c76cc52eb2ae6069e02042e5db44cb886aa0
ritika <ritika@mozzarella.ict.kth.se>
2023-03-10 18:09:53 +0100

    0.7% error!

--- code/power.py
+++ code/power.py
@@ -1,9 +1,7 @@
 class Power():
     def __init__(self):
         self.POWER_FILE = ""
-        self.nets = []
-        self.inactive_nets = []
-        self.active_nets = []
+        self.nets = {}
 
     def set_nets(self,powerfile):
         self.POWER_FILE = powerfile
@@ -13,99 +11,89 @@ class Power():
             lines = file.readlines()[header:-tail]
             for line in lines:
                 line = " ".join(line.split()).split(" ")
-                line[5] = 'inactive'
-                self.nets.append(line)
-
-    def set_active_nets_legacy(self,signals):
-        for signal in signals:
-            all_module_signals = signal.split('*')
-            for net in self.nets:
-                net_name = net[0]
-                if all(module_signal in net_name for module_signal in all_module_signals):
-                    self.active_nets.append(net)
+                name = line[0]
+                if name in self.nets:
+                    label = self.nets[name]['label']
+                else:
+                    label = 'inactive'
+                self.nets[name] = {'internal' : float(line[2]), 'switching' : float(line[3]), 'leakage' : float(line[4]), 'label' : label}
+        inactive = 0
+        total = 0
+        active = 0
+        for name in self.nets:
+            net = self.nets[name]
+            label = net['label']
+            if (label == 'inactive'):
+                inactive += 1
+            else:
+                active += 1
+            total += 1
+#        print("Active: ", active, " Inactive: ", inactive, ' added: ', inactive + active, ' Total: ', total)
 
     def set_active_nets(self,signals):
+        count = 0
         for signal in signals:
-            all_module_signals = signal.split('*')
-            for i in range(len(self.nets)):
-                net = self.nets[i]
-                net_name = net[0]
-                if all(module_signal in net_name for module_signal in all_module_signals):
-                    self.nets[i][5] = 'active'
+            signal_substrings = signal.split('*')
+            for name in self.nets:
+                net = self.nets[name]
+                label = net['label']
+                if (label == 'inactive'):
+                    if all(substring in name for substring in signal_substrings):
+                        self.nets[name]['label'] = signal
+                        count += 1
 
-    def set_inactive_nets(self):
-        for net in self.nets:
-            if net not in self.active_nets:
-                self.inactive_nets.append(net)
-
-    def get_active_component_power_legacy(self):
-        internal_power = 0
-        switching_power = 0
-        leakage_power = 0
-        for net in self.active_nets:
-            internal_power += float(net[2])
-            switching_power += float(net[3])
-            leakage_power += float(net[4])
+    def get_active_component_power(self, signals):
+        count = 0
+        for signal in signals:
+            internal_power = 0
+            switching_power = 0
+            leakage_power = 0
+            for name in self.nets:
+                net = self.nets[name]
+                if(net['label'] == signal):
+                    internal_power += net['internal']
+                    switching_power += net['switching']
+                    leakage_power += net['leakage']
+                    count += 1
         power = {'internal': internal_power, 
                     'switching': switching_power,
                     'leakage': leakage_power
                     }
-        return(power)
-
-    def get_active_component_power(self):
-        internal_power = 0
-        switching_power = 0
-        leakage_power = 0
-        for net in self.nets:
-            if(net[5] == 'active'):
-                internal_power += float(net[2])
-                switching_power += float(net[3])
-                leakage_power += float(net[4])
-        power = {'internal': internal_power, 
-                    'switching': switching_power,
-                    'leakage': leakage_power
-                    }
-        return(power)
-
-    def get_inactive_component_power_legacy(self):
-        internal_power = 0
-        switching_power = 0
-        leakage_power = 0
-        for net in self.inactive_nets:
-            internal_power += float(net[2])
-            switching_power += float(net[3])
-            leakage_power += float(net[4])
-        power = {'internal': internal_power, 
-                    'switching': switching_power,
-                    'leakage': leakage_power
-                    }
-        return(power)
+        return(power,count)
 
     def get_inactive_component_power(self):
         internal_power = 0
         switching_power = 0
         leakage_power = 0
-        for net in self.nets:
-            if(net[5]== 'inactive'):
-                internal_power += float(net[2])
-                switching_power += float(net[3])
-                leakage_power += float(net[4])
+        count = 0
+        for name in self.nets:
+            net = self.nets[name]
+            if(net['label'] == 'inactive'):
+                internal_power += net['internal']
+                switching_power += net['switching']
+                leakage_power += net['leakage']
+                count += 1
         power = {'internal': internal_power, 
                     'switching': switching_power,
                     'leakage': leakage_power
                     }
-        return(power)
+#        print("Inactive net count: ", count)
+        return(power,count)
 
     def get_total_power(self):
         internal_power = 0
         switching_power = 0
         leakage_power = 0
-        for net in self.nets:
-            internal_power += float(net[2])
-            switching_power += float(net[3])
-            leakage_power += float(net[4])
+        count = 0
+        for name in self.nets:
+            net = self.nets[name]
+            internal_power += net['internal']
+            switching_power += net['switching']
+            leakage_power += net['leakage']
+            count += 1
         power = {'internal': internal_power, 
                     'switching': switching_power,
                     'leakage': leakage_power
                     }
-        return(power)
+#        print("Total net count: ", count)
+        return(power,count)
