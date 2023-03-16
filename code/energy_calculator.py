8a85960983be1ae9edbc2f0964552f749ac32c40
ritika <ritika@mozzarella.ict.kth.se>
2023-03-16 14:01:23 +0100

    no error for the first sanity check!

--- code/energy_calculator.py
+++ code/energy_calculator.py
@@ -7,14 +7,18 @@ class EnergyCalculator():
         self.inactive_windows = inactive_windows
         self.total_window = total_window
         self.NetPower = Power()
+
         self.active_component_power = {}
         self.inactive_component_power = {} 
         self.total_power = {}
+
         self.active_component_energy = {}
         self.inactive_component_energy = {} 
+        self.estimated_energy = {} 
         self.total_energy = {}
         self.energy = {}
         self.energy_error = {}
+
         self.net_count = {}
         self.active_net_count = 0
         self.inactive_net_count = 0
@@ -24,37 +28,35 @@ class EnergyCalculator():
         self.set_energy()
         self.print_energy()
 
-    def print_power(self):
-        for component in self.power:
-            for state in self.power[component]:
-                for duration in self.power[component][state]:
-                    for power_type in self.power[component][state][duration]:
-                        print(component,state,duration,power_type,round(self.power[component][state][duration][power_type],2))
+    def set_active_window_power(self,component):
+        signals = self.active_components[component]
+        print('Active power for: ',component ,' ', signals)
+        for window in self.active_windows[component]:
+            active_component_active_power_file = self.tb + "/vcd/" + component + "_active_" + str(window['start']) + "_" + str(window['end']) + ".vcd.pwr"
+            self.NetPower.set_nets(active_component_active_power_file)
+            self.NetPower.set_active_nets(signals)
+            duration = str(window['start']) + "_" + str(window['end']) 
+            self.active_component_power[component]['active'][duration], self.net_count[component] = self.NetPower.get_active_component_power(signals)
+
+    def set_inactive_window_power(self,component):
+        signals = self.active_components[component]
+        print('Inactive power for: ',component ,' ', signals)
+        for window in self.inactive_windows[component]:
+            active_component_inactive_power_file = self.tb + "/vcd/" + component + "_inactive_" + str(window['start']) + "_" + str(window['end']) + ".vcd.pwr"
+    #       print(component,signals,window)
+            self.NetPower.set_nets(active_component_inactive_power_file)
+            self.NetPower.set_active_nets(signals)
+            duration = str(window['start']) + "_" + str(window['end']) 
+            self.active_component_power[component]['inactive'][duration], self.net_count[component] = self.NetPower.get_active_component_power(signals)
 
     def set_active_component_power(self):
         print("Computing active component power... ")
         for component in self.active_components:
             self.net_count[component] = 0
             self.active_component_power[component] = {'active': {}, 'inactive': {}}
-            signals = self.active_components[component]
-            print('Component: ',component ,' ', signals)
-            for window in self.active_windows[component]:
-                active_component_active_power_file = self.tb + "/vcd/" + component + "_active_" + str(window['start']) + "_" + str(window['end']) + ".vcd.pwr"
-                self.NetPower.set_nets(active_component_active_power_file)
-                self.NetPower.set_active_nets(signals)
-                duration = str(window['start']) + "_" + str(window['end']) 
-                self.active_component_power[component]['active'][duration], self.net_count[component] = self.NetPower.get_active_component_power(signals)
-    #            print(component,'active', duration,self.active_component_power[component]['active'][duration])
-            for window in self.inactive_windows[component]:
-                active_component_inactive_power_file = self.tb + "/vcd/" + component + "_inactive_" + str(window['start']) + "_" + str(window['end']) + ".vcd.pwr"
-    #            print(component,signals,window)
-                self.NetPower.set_nets(active_component_inactive_power_file)
-                self.NetPower.set_active_nets(signals)
-                duration = str(window['start']) + "_" + str(window['end']) 
-                self.active_component_power[component]['inactive'][duration], self.net_count[component] = self.NetPower.get_active_component_power(signals)
+            self.set_active_window_power(component)
+            self.set_inactive_window_power(component)
             self.active_net_count += self.net_count[component]
-#            print("Active net count over all seen nets: ", self.net_count[component], " / ", self.active_net_count)
-    #            print(component,'inactive', duration,self.active_component_power[component]['inactive'][duration])
 
     def set_inactive_component_power(self):
         print("Computing inactive component power... ")
@@ -67,26 +69,35 @@ class EnergyCalculator():
         self.inactive_component_power, self.inactive_net_count = self.NetPower.get_inactive_component_power()
 
     def set_total_power(self):
-        print("Computing total component power.. ")
+        print("Computing total component power... ")
+        active_power = {'internal': 0, 'switching': 0, 'leakage': 0}
+        self.total_power = {'internal': 0, 'switching': 0, 'leakage': 0}
         power_file = self.tb + "/vcd/" + "total" + "_active_" + str(self.total_window['start']) + "_" + str(self.total_window['end']) + ".vcd.pwr"
-        duration = str(self.total_window['start']) + "_" + str(self.total_window['end']) 
-        self.NetPower.set_nets(power_file)
-        self.total_power, self.total_net_count = self.NetPower.get_total_power()
+        for component in self.active_components:
+            signals = self.active_components[component]
+            print('Actual power for: ',component ,' ', signals)
+            self.NetPower.set_nets(power_file)
+            self.NetPower.set_active_nets(signals)
+            component_power, self.net_count[component] = self.NetPower.get_active_component_power(signals)
+            for power_type in component_power:
+                active_power[power_type] += component_power[power_type]
+
+        for power_type in self.total_power:
+            self.total_power[power_type] = self.inactive_component_power[power_type] + active_power[power_type]              #The inactive power is independent of windows
 
     def set_power(self):
         self.set_active_component_power()
         self.set_inactive_component_power()
         self.set_total_power()
+        self.total_net_count = self.active_net_count + self.inactive_net_count
         print('Net count... :')
-        print('Active components: ', self.active_net_count,' Inactive components: ', self.inactive_net_count,' Total: ', self.total_net_count, ' Error: ', self.total_net_count - (self.active_net_count + self.inactive_net_count))
+        print('Active components: ', self.active_net_count,' Inactive components: ', self.inactive_net_count,' Total: ', self.total_net_count)
 
     def set_active_component_energy(self):
+        print("Computing active component energy... ")
         self.active_component_energy = {'internal': 0, 'switching': 0, 'leakage' : 0}
-        internal = 0
-        switching = 0
-        leakage = 0
         print('Active component, state, duration:')
-        for component in self.active_component_power:
+        for component in self.active_components:
             self.energy[component] = {'active': {}, 'inactive': {}}
             for state in self.active_component_power[component]:
                 for duration in self.active_component_power[component][state]:
@@ -99,8 +110,8 @@ class EnergyCalculator():
                         self.active_component_energy[power_type] += self.energy[component][state][duration][power_type]
 #                    print(component, state, duration,self.energy[component][state][duration])
 
-
     def set_inactive_component_energy(self):
+        print("Computing inactive component energy... ")
         self.inactive_component_energy = {'internal': 0, 'switching': 0, 'leakage' : 0}
         start = self.total_window['start']
         end = self.total_window['end']
@@ -109,6 +120,7 @@ class EnergyCalculator():
             self.inactive_component_energy[power_type] = time * self.inactive_component_power[power_type]
 
     def set_total_energy(self):
+        print("Computing total component energy... ")
         self.total_energy = {'internal': 0, 'switching': 0, 'leakage' : 0}
         start = self.total_window['start']
         end = self.total_window['end']
@@ -121,6 +133,7 @@ class EnergyCalculator():
         for power_type in self.total_power:
             estimate = self.active_component_energy[power_type] + self.inactive_component_energy[power_type]
             actual = self.total_energy[power_type]
+            self.estimated_energy[power_type] = estimate
             self.energy_error[power_type] = (actual - estimate) * 100 /max(actual,estimate)
 
     def set_energy(self):
@@ -130,7 +143,6 @@ class EnergyCalculator():
         self.set_energy_error()
 
     def print_energy(self):
-        print("total: ", 'internal : ', self.total_energy['internal'], ' switching : ',self.total_energy['switching'],  ' leakage : ',self.total_energy['leakage'])
-        print("active_component_energy: ", 'internal : ', self.active_component_energy['internal'], ' switching : ',self.active_component_energy['switching'],  ' leakage : ',self.active_component_energy['leakage'])
-        print("inactive_component_energy: ", 'internal : ', self.inactive_component_energy['internal'], ' switching : ',self.inactive_component_energy['switching'],  ' leakage : ',self.inactive_component_energy['leakage'])
+        print("Actual: ", 'internal : ', self.total_energy['internal'], ' switching : ',self.total_energy['switching'],  ' leakage : ',self.total_energy['leakage'])
+        print("Estimate: ", 'internal : ', self.estimated_energy['internal'], ' switching : ',self.estimated_energy['switching'],  ' leakage : ',self.estimated_energy['leakage'])
         print("Error: ", 'internal : ', self.energy_error['internal'], ' switching : ',self.energy_error['switching'],  ' leakage : ',self.energy_error['leakage'])
